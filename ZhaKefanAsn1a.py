07438664c19990255b3d761baed2a7f289189b00
kefanz <kzha@fordham.edu>
2020-02-10 19:07:28 -0500

    进行本次更新——first

---
+++ ZhaKefanAsn1a.py
@@ -0,0 +1,7 @@
+#<Kefan><Zha>Assignment<1><A>
+
+running_time = float(input('How far will you run (in miles)? '))
+fast = float(input('How fast will you run(in minutes/mile; enter decimal, like 7.5 for 7:30 pace)? '))
+minutes = running_time * fast
+miles = running_time
+print('It will take you', minutes, 'minutes to run', miles, 'miles')
